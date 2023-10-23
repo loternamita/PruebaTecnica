0f29609d37591086385369f0b3c73ad294c635e1
Project Docker <g_andres1398@outlook.com>
2023-10-23 12:58:57 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ export class AppComponent {
   valorNuevo = 'Hola mundooooo';
   mensajeBienvenida = '¡Bienvenido a mi aplicación Angular!';
   fechaActual = new Date();
-  valorNuevo20 = '18.0';
+  valorNuevo20 = '19.0';
 }
